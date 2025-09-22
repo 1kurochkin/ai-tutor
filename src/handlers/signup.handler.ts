fc2eca718231494ee013a05f2f9c4d6e41199b10
1kurochkin <contact@ikurochkin.com>
2025-09-22 11:56:49 -1000

    fix warnings

--- src/handlers/signup.handler.ts
+++ src/handlers/signup.handler.ts
@@ -1,5 +1,5 @@
 "use server";
-import { SignupFormValues } from "../app/(auth)/signup/page";
+import { SignupFormValues } from "@/app/(auth)/signup/page";
 const signupHandler = async (values: SignupFormValues) => {
   await new Promise((resolve) => setTimeout(resolve, 4000));
   console.log(values);
