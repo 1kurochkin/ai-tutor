95e98ae250657a5486e1568c709a314c22ba8bc4
1kurochkin <contact@ikurochkin.com>
2025-09-22 17:01:37 -1000

    adjust login handler to utilize route api

--- src/handlers/login.handler.ts
+++ src/handlers/login.handler.ts
@@ -2,8 +2,17 @@
 import { LoginFormValues } from "@/app/(auth)/login/page";
 
 const loginHandler = async (values: LoginFormValues) => {
-  await new Promise((resolve) => setTimeout(resolve, 4000));
-  console.log(values);
+  console.log("loginHandler", values);
+  const response = await fetch("http://localhost:3000/api/auth/login", {
+    method: "POST",
+    body: JSON.stringify(values),
+  });
+  const data = await response.json();
+  console.log("loginHandler DATA", data);
+  if (!response.ok) {
+    throw new Error(data.error || "Failed to login. Please try again later.");
+  }
+  return data;
 };
 
 export default loginHandler;
