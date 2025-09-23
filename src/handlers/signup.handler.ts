9f7c94968a80291d301b73ce6edb0abb5a1dd2da
1kurochkin <contact@ikurochkin.com>
2025-09-22 16:49:35 -1000

    adjust signupHandler to use api

--- src/handlers/signup.handler.ts
+++ src/handlers/signup.handler.ts
@@ -1,8 +1,19 @@
 "use server";
 import { SignupFormValues } from "@/app/(auth)/signup/page";
-const signupHandler = async (values: SignupFormValues) => {
-  await new Promise((resolve) => setTimeout(resolve, 4000));
-  console.log(values);
+const signupHandler = async (
+  values: Omit<SignupFormValues, "confirmPassword">,
+) => {
+  console.log("signupHandler", values);
+  const response = await fetch("/api/auth/signup", {
+    method: "POST",
+    body: JSON.stringify(values),
+  });
+  const data = await response.json();
+  console.log("signupHandler DATA", data);
+  if (!response.ok) {
+    throw new Error(data.error || "Failed to signup. Please try again later.");
+  }
+  return data;
 };
 
 export default signupHandler;
