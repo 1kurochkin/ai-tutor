9053981be4397b836c62a5c99939961b696d8ce6
1kurochkin <contact@ikurochkin.com>
2025-09-23 08:41:21 -1000

    put AuthChecker comp to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import AuthChecker from "@/components/auth-check";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <AuthChecker />
         {children}
         <Toaster />
       </body>
