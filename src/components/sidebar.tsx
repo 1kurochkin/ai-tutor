fe3b024e8834711d313386b89c44a56b12e0ad93
1kurochkin <contact@ikurochkin.com>
2025-09-23 21:15:36 -1000

    feature-toggle-sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,36 +1,63 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { Chat } from "@prisma/client";
+'use client'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { Chat } from '@prisma/client'
+import { usePathname } from 'next/navigation'
+import { MenuIcon } from 'lucide-react'
+import { useState } from 'react'
 
-export default async function Sidebar({ chats }: { chats: Partial<Chat>[] }) {
-  console.log(chats, "Sidebar");
+export default function Sidebar({ chats }: { chats: Partial<Chat>[] }) {
+  console.log(chats, 'Sidebar')
+  const pathname = usePathname()
+  const [isOpen, setIsOpen] = useState(true)
+  const handleToggleSidebar = () => {
+    console.log('handleToggleSidebar')
+    setIsOpen(prev => !prev)
+  }
   return (
-    <div className="w-64 bg-black text-white flex flex-col transition-all duration-300 p-4 gap-4">
-      <Button asChild variant="outline">
-        <Link href="/chat">New Chat</Link>
-      </Button>
-
-      <div className="mt-8 overflow-y-auto h-full flex flex-col gap-2">
-        {chats.length === 0 ? (
-          <span>Chats</span>
-        ) : (
-          chats.map((chat) => (
-            <Link
-              key={chat.id}
-              href={`/chat/${chat.id}`}
-              className="p-2 rounded-md hover:bg-gray-700 transition"
-            >
-              {chat.title || "Untitled Chat"}
-            </Link>
-          ))
+    <div
+      className={`${!isOpen ? 'w-20' : 'w-64'} bg-black text-white flex flex-col transition-all duration-300 p-4 gap-4`}>
+      <div className={'flex justify-between items-center'}>
+        {isOpen && (
+          <Button asChild variant="outline">
+            <Link href="/chat">New Chat</Link>
+          </Button>
         )}
+        <Button onClick={handleToggleSidebar} variant={'outline'}>
+          <MenuIcon className={'text-black'} />
+        </Button>
       </div>
 
-      <form action="http://localhost:3000/api/auth/logout" method="post">
-        <Button className={"w-full"} type="submit" variant="outline">
-          Logout
-        </Button>
-      </form>
+      {isOpen && (
+        <>
+          <div className="mt-8 overflow-y-auto h-full flex flex-col gap-2">
+            {chats.length === 0 ? (
+              <span>Chats</span>
+            ) : (
+              chats.map(chat => (
+                <Button
+                  asChild
+                  key={chat.id}
+                  className={
+                    pathname.includes(chat.id || '') ? 'bg-gray-700' : ''
+                  }>
+                  <Link href={`/chat/${chat.id}`}>
+                    {chat.title || 'Untitled Chat'}
+                  </Link>
+                </Button>
+              ))
+            )}
+          </div>
+
+          <form
+            action="http://localhost:3000/api/auth/logout"
+            method="post">
+            <Button className={'w-full'} type="submit" variant="outline">
+              Logout
+            </Button>
+          </form>
+        </>
+      )}
     </div>
-  );
+  )
 }
