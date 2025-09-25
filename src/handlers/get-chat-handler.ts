61ade92da4fe8837113c46439bbda5c0541bb619
1kurochkin <contact@ikurochkin.com>
2025-09-25 12:09:31 -1000

    prettier

--- src/handlers/get-chat-handler.ts
+++ src/handlers/get-chat-handler.ts
@@ -1,45 +1,46 @@
 'use server'
 
-import { cookies } from 'next/headers'
+import {cookies} from 'next/headers'
 import jwt from 'jsonwebtoken'
-import { prisma } from '@/lib/prisma'
-import { redirect } from 'next/navigation'
+import {prisma} from '@/lib/prisma'
+import {redirect} from 'next/navigation'
 
 const getChatHandler = async (chatId: string) => {
-  console.log('getChatHandler', chatId)
-  const cookieStore = await cookies()
-  const token = cookieStore.get('token')?.value
+    console.log('getChatHandler', chatId)
+    const cookieStore = await cookies()
+    const token = cookieStore.get('token')?.value
 
-  if (!token) {
-    redirect('/')
-  }
-
-  let payload: { userId: string }
-  try {
-    payload = jwt.verify(token, process.env.JWT_SECRET!) as {
-      userId: string
+    if (!token) {
+        redirect('/')
     }
-  } catch (err) {
-    console.error('Invalid token', err)
-    throw new Error('Unauthorized: Invalid token')
-  }
-  return prisma.chat.findUnique({
-    where: { id: chatId, userId: payload.userId },
-    include: {
-      file: {
-        select: {
-          id: true,
-          originalName: true,
-          url: true,
-          pageCount: true,
-          extractedText: true,
+
+    let payload: { userId: string }
+    try {
+        payload = jwt.verify(token, process.env.JWT_SECRET!) as {
+            userId: string
+        }
+    } catch (err) {
+        console.error('Invalid token', err)
+        throw new Error('Unauthorized: Invalid token')
+    }
+    return prisma.chat.findUnique({
+        where: {
+            id: chatId,
+            userId: payload.userId
         },
-      },
-      messages: {
-        orderBy: { createdAt: 'asc' },
-      },
-    },
-  })
+        include: {
+            file: {
+                select: {
+                    id: true,
+                    originalName: true,
+                    url: true,
+                    pageCount: true,
+                    extractedText: true,
+                },
+            },
+            messages: true,
+        },
+    })
 }
 
 export default getChatHandler
