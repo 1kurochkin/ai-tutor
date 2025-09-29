7c69d6e0a21230c49a5ea5389b52c80becc3de46
1kurochkin <contact@ikurochkin.com>
2025-09-28 19:00:52 -1000

    eslint

--- src/app/api/auth/user/route.ts
+++ src/app/api/auth/user/route.ts
@@ -1,24 +1,21 @@
-import {NextRequest, NextResponse} from "next/server";
-import { prisma } from "@/lib/prisma";
-import bcrypt from "bcrypt";
-import {generateToken, getUserFromToken, setTokenCookie} from "@/lib/auth";
-
+import { NextRequest, NextResponse } from 'next/server'
+import { getUserFromToken, setTokenCookie } from '@/lib/auth'
 
 export async function GET(req: NextRequest) {
   // Check authentication
-  console.log("GET ME")
+  console.log('GET ME')
   const token = req.cookies.get('token')?.value
   if (!token) {
-    return NextResponse.json({error: 'Unauthorized'}, {status: 401})
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
   }
   console.log(token, 'TOKEN')
   const user = await getUserFromToken(token)
   console.log(user, 'USER')
   if (!user) {
-    return NextResponse.json({error: 'Invalid token'}, {status: 401})
+    return NextResponse.json({ error: 'Invalid token' }, { status: 401 })
   }
 
-  const res = NextResponse.json(user);
+  const res = NextResponse.json(user)
 
-  return setTokenCookie(res, token);
+  return setTokenCookie(res, token)
 }
